5c93d6a4af6f162d7c481e5f6d85521761113e6e
LucVanThe <01672161375pk@gmail.com>
2025-06-20 17:50:14 +0700

    The
    Shop online

---
+++ Models/OrderDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopOnline.Models;
+
+public partial class OrderDetail
+{
+    public int OrderDetailId { get; set; }
+
+    public int? OrderId { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public string? OrderNumber { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public double? Discount { get; set; }
+
+    public double? Total { get; set; }
+
+    public DateTime? ShipDate { get; set; }
+
+    public virtual Order? Order { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Models;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
+
+// 🔹 Lấy chuỗi kết nối
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 🔹 Đăng ký DbContext
+builder.Services.AddDbContext<WebBanhangDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddSingleton<HtmlEncoder>(HtmlEncoder.Create(allowedRanges: new[] { UnicodeRanges.All }));
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapAreaControllerRoute(
+    name: "areas",
+    areaName: "Admin",
+    pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
