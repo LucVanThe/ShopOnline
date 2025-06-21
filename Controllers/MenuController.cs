6fccedd8931560028ab41468ceb5939214a45fd4
LucVanThe <01672161375pk@gmail.com>
2025-06-21 19:03:04 +0700

    The

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopOnline.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MenuArrivals()
+        {
+            return View();
+        }
+    }
+}
